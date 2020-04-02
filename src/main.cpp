eac558bf79c2fffc6bd9b3d653963f17a53459e4
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-04-03 01:41:12 +0200

    added example app (stackwalker)

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+/*
+# Breakdown <https://github.com/rodlie/breakdown>
+#
+# Copyright (c) 2020, Ole-André Rodlie <ole.andre.rodlie@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <breakdown.h>
+#include <iostream>
+
+int main(int argc, char** argv) 
+{ 
+    if (argc == 3) {
+        std::string folder = argv[1];
+        std::string file = argv[2];
+        if (!folder.empty() && !file.empty()) {
+            std::vector<std::string> storage;
+            storage.push_back(folder);
+            std::string result = BreakDown::convertDumpToString(file, storage);
+            if (!result.empty()) {
+                std::cout << result << std::endl;
+                return 0;
+            }
+        }
+    }
+    std::cout << "Usage: stackwalker <SYMBOLS_STORAGE> <CRASH_DUMP>" << std::endl;
+    return 1;
+}
